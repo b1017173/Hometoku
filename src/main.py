7388afccb06843f88563996f497d5b0fc91ca8f3
b1017173 <g2121034@fun.ac.jp>
2021-06-15 21:01:23 +0900

    [add]サンプルコードの追加

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-def main():
-    pass
+import os
+# Use the package we installed
+from slack_bolt import App
 
-if __name__ == '__main__':
-    main()
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# Add functionality here
+# @app.event("app_home_opened") etc
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
