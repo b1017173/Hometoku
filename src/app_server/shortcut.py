0fdc2fa7848d629d142a4fce3be58df6d793d95b
b1017173 <g2121034@fun.ac.jp>
2021-06-18 02:17:29 +0900

    前回のできてなかったみたい

---
+++ src/app_server/shortcut.py
@@ -0,0 +1,28 @@
+def view_modal_from_shortcut(client, shortcut):
+    # モーダル表示のリクエスト
+    client.views_open(
+        trigger_id = shortcut["trigger_id"],
+        view = {
+            "type": "modal",
+            "title": {"type": "plain_text", "text":"My App"},
+            "close": {"type": "plain_text", "text":"Close"},
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text":"About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text":"Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+                        }
+                    ]
+                }
+            ]
+        }
+    )
