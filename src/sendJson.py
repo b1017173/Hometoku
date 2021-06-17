5db03a93b453a934541910232c43b300f5c7422e
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-06-18 02:06:12 +0900

    [add] 送信用のJsonファイルを作成できたけどコードがうんこ

--- src/sendJson.py
+++ src/sendJson.py
@@ -1,5 +1,6 @@
 import json
 
+
 class SendJson:
 
     def __init__(self) -> None:
@@ -12,18 +13,24 @@ class SendJson:
 
         home_targets = catch_json_read["homePeople"]["multi_users_select-action"]["selected_users"] #  褒める相手を抽出
         home_contents = catch_json_read["homeMove"]["plain_text_input-action"]["value"]  #  褒め内容を抽出
-        # 褒め尺度 home_scale = 
 
-        send_json_contents = dict()
-        send_json_contents['homePeople'] = home_targets
-        send_json_contents['homeMove'] = home_contents
+        home_data = dict()  #  必要な要素だけを入れておくdict
+        home_data["homePeople"] = home_targets  #  褒める相手の要素を入れる
+        home_data["homeMove"] = home_contents  #  褒め内容を入れる
 
-        print(send_json_contents)
-
-    def __object_json(self,json_data:dict):
-        return a
+        with open('data/send_test.json','w') as send_json:
+            json.dump(home_data, send_json, ensure_ascii=False, indent=2)
 
 '''
+    #  必要な要素（褒めたい相手，褒め内容，褒め尺度）を抽出
+    def __element_extraction(self,json_data:dict):
+        #home_people = json_box.homePeople.multi_users_select-action.selected_users
+        #home_contents = json_box.homeMove.plain_text_input-action.value
+        # 褒め尺度 home_scale = 
+
+        #need_element = dict()
+        #need_element.update(home_people,home_contents)
+
     #  SlackAppが表示する情報のみを抽出
     def __extract_data(self,catch_json_data):
 
