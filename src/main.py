79961edfbe4a1d390f4fe8dd3ff41211669efce2
Micad <43740965+b1017173@users.noreply.github.com>
2021-06-19 02:30:09 +0900

    Revert "モーダルの送信ボタンを押した時のリスナーを実装"

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import os
 # Use the package we installed
 from slack_bolt import App
-import app_server.shortcut as sc
 
 # Initializes your app with your bot token and signing secret
 app = App(
@@ -9,31 +8,9 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# 'shortcut_homeru' という callback_id のショートカットをリッスン
-@app.shortcut("shortcut_homeru")
-def open_modal(ack, shortcut, client):
-    # リクエストを受け付け
-    ack()
-    sc.view_modal_from_shortcut(client, shortcut)
+# Add functionality here
+# @app.event("app_home_opened") etc
 
-# 'homeru'モーダルを Submit したことをリッスン
-@app.view("modal_homeru")
-def handle_submission(ack, body, client, view, logger):
-    ack()
-    _user = body["user"]["id"]                                                              # 投稿ユーザ
-    _targets = view["state"]["values"]["homepeople"]["select_homepeople"]["selected_users"] # 褒めたい人・チャンネル
-    _prise_writing = view["state"]["values"]["homemove"]["input_homemove"]["value"]         # 褒めたいこと
-    print("user: ", _user)
-    print("targets: ", _targets)
-    print("prise writing: ", _prise_writing)
-    # _prise_quantity = view["state"]["values"]["blockID"]["actionID"]
-    
-    # メッセージ送信の関数
-    # xx.yyyyy(client, logger, _user, _targets, _prise_writing)
-    # xx.yyyyy(client, logger, _user, _targets, _prise_writing, _prise_quantity)
-
-    # DBへの書き込み
-    # xx.yyyy(_targets, _prise_quantity)
 
 # Start your app
 if __name__ == "__main__":
