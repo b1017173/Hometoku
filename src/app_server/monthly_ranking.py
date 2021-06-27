2532356803ab0fd6d9b152397512fe2b780e7951
KenSato0527 <satoken527@gmail.com>
2021-06-27 23:15:58 +0900

    [add] 月間ランキング用の関数作成中(DBがまだ)

--- src/app_server/monthly_ranking.py
+++ src/app_server/monthly_ranking.py
@@ -1,16 +1,15 @@
-def monthly_ranking(): #褒められた回数top3までを表彰
-     """褒められた回数でソート
+def post_monthlyranking(say:function): #褒められた回数を各ワークスペースtop3までを表彰
+     """1.workspace_idとchannel_idのテーブルをみて，workspace_idで褒めたときにsubmitされたレコードを抽出
+        2.褒められた回数でソート
+        3.top3までjsonでworkspace_idのchannel_idのとこに送信(誰もいなかったら煽るか)
+        123をworkspace_idテーブルの長さ分ぶんまわす"""
+     #オーダーバイカラム名で褒められ度でソートしてリミット３とかでひっぱてこれるらしいぞ
+     _workspaceList = [] #workspace_idとchannel_idのリスト
      
-     _ranking +=  {
-                 "type": "section",
-                 "text": {
-                     "type": "mrkdwn",
-                     "text": "*1位 ユーザーid*\n:clap::clap::clap: 褒めた回数 ホメ\n"
-                 }
-             },
-"""
 
-     return {
+     for idList in _workspaceList: #[0]:workspace_id [1]: channel_id
+         _rankingList = [] #ワークスペース毎の褒められランキングのリスト(1で抽出されたやつ)
+         view = {
 	     "blocks": [
 		     {
 			     "type": "header",
@@ -30,33 +29,21 @@ def monthly_ranking(): #褒められた回数top3までを表彰
              {
                  "type": "divider"
              },
-             {
-                 "type": "section",
-                 "text": {
-                     "type": "mrkdwn",
-                     "text": "*1位 ユーザーid*\n:clap::clap::clap: 褒めた回数 ホメ\n"
-                 }
-             },
-             {
-                 "type": "section",
-                 "text": {
-                     "type": "mrkdwn",
-                     "text": "*2位 ユーザーid*\n:clap::clap::clap: 褒めた回数 ホメ"
-                 }
-             },
-             {
-                 "type": "section",
-                 "text": {
-                     "type": "mrkdwn",
-                     "text": "*3位 かい*\n:clap::clap: 1 ホメ\n"
-                 }
-             },
-             {
-                 "type": "divider"
-             }
          ]
      }
 
+         for rank in range(0,len(_rankingList)):
+             _clapCount = ":clap:"*(3-rank)
+             _ranking =  {
+                     "type": "section",
+                     "text": {
+                         "type": "mrkdwn",
+                         "text": "*{0}位 {1}*\n{2} 褒めた回数 {3}ホメ\n".format(rank+1,_rankingList[rank]["""user_idのとこ"""],_clapCount,_rankingList[rank]["""褒められた回数のとこ"""])
+                     }
+                 },
+             view["blocks"].append(_ranking)
+
+         say(view,channel = idList[1])
 """
      client.chat_postMessage(
          channel="C025BBH57LN",
