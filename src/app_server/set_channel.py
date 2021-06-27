069b6e9295fc6024d9ecf0f72c7ada6c431afef1
Kai-Moriuchi <g2121053@fun.ac.jp>
2021-06-27 21:20:03 +0900

    [fix] プルリクでもらったコメントの反映

--- src/app_server/set_channel.py
+++ src/app_server/set_channel.py
@@ -1,6 +1,9 @@
-# コマンドによってチャンネルに参加する
+# コマンドによってチャンネルに参加する(参加できるのは1つのチャンネルだけ)
 def set_channel(say, command, client):
-    _channel_id = command["channel_id"]
+    _channel_id = command["channel_id"] # コマンドが呼ばれたチャンネルID用の変数
+    _user_id = command["user_id"] # コマンドを呼び出した人のユーザーID用の変数
+    _joined_channel_id:str = ""  # 既に参加しているチャンネルID用の変数
+
     #  参加した時に表示させるMessagePreview
     _success_message = {
 	        "blocks": [
@@ -27,7 +30,7 @@ def set_channel(say, command, client):
 			        "fields": [
 				        {
 					        "type": "mrkdwn",
-					        "text": "ごめんなさい....\nもう他のチャンネルにいるんだよね....\n#channelで呼んでもらってもいいかな....??"
+					        "text": "ごめんなさい....\nもう他のチャンネルにいるんだよね....\n<#{0}>で呼んでもらってもいいかな....??".format(_joined_channel_id)
 				        }
 			        ]
 		        },
@@ -40,14 +43,15 @@ def set_channel(say, command, client):
         }
 
     try:
-        ## TODO: データベースにアクセスしてすでに参加しているチャンネルがないかを調べる
+        # TODO: DBにアクセスしてすでに参加しているチャンネルがないかを調べる
         # if 参加している:
-        #   TODO: すでに参加している系のメッセージを送る
-        # say(text = _failure_message, channel = _channel_id)  #  コマンドが入力されたチャンネルにすでに参加してるメッセージを送信
+		# TODO:すでに参加しているチャンネルIDをDBから取得して _joined_channel_idに保存
+		# コマンドを呼び出した人だけに見えるメッセージを送信
+        # client.chat_postEphemeral(text = _failure_message, channel = _channel_id, user = _user_id)  
         # else:
-        #   TODO: 参加させる(実装済)+参加したよのメッセージ送る
-        client.conversations_join(channel = _channel_id)
+        client.conversations_join(channel = _channel_id)  #  コマンドが入力されたチャンネルに参加する
         say(text = _success_message, channel = _channel_id)  #  コマンドが入力されたチャンネルに参加したよのメッセージ送信
+		# TODO: DBにBotが参加したワークスペースID，チャンネルIDを追加する
 
     except Exception as e:
         print("Error: Failed to join the channel.\n{0}".format(e))
